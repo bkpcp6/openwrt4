4872f443043dfec5c6f2698a81aeecfcbbeac9b2
bkpcp6 <111767403+bkpcp6@users.noreply.github.com>
2022-08-27 09:22:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt /bkpcp compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g' $ZZZ
